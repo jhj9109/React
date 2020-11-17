0f423aa080869251d8fa7b25f89019128f3e2d86
jhj9109 <jhj91_09@naver.com>
2020-11-17 13:04:41 +0900

    Update { 비구조화 } = useContext(해당Context)

--- src/components/ColorBox2.js
+++ src/components/ColorBox2.js
@@ -1,14 +1,10 @@
-import React from "react";
-import { ColorConsumer } from "../contexts/color2";
+import React, { useContext } from "react";
+import ColorContext2, { ColorConsumer } from "../contexts/color2";
+
 const ColorBox2 = () => {
+  const { state } = useContext(ColorContext2)
   return (
-    <ColorConsumer>
-      { ({ state }) =>{
-        console.log("ColorBox2", state)
-        return <div style={{ width: "400px", height: "400px", background: state.color }} />
-      } 
-      }
-    </ColorConsumer>
+    <div style={{ width: "400px", height: "400px", background: state.color }} />
   );
 };
 
